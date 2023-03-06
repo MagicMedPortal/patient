a1f6f39c09d9c39674d58898ce9fe521f2965788
ish <ish@ishtotheizzo>
2023-03-06 14:59:55 -0700

    try to add p5 js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from 'react'
-import Sketch from 'react-p5'
 
-function App() {
-  const setup = (p5, canvasParentRef) => {
-    p5.createCanvas(500, 400).parent(canvasParentRef)
-  }
-  
-  const draw = p5 => {
-    p5.background(255, 130, 20)
-    p5.ellipse(100, 100, 100)
-    p5.ellipse(300, 100, 100)
-  }
-  
-  return <Sketch setup={setup} draw={draw} />
+import React from "react";
+import Sketch from "react-p5";
+ 
+
+const setup = (p5, canvasParentRef) => {
+  p5.createCanvas(window.innerWidth, window.innerHeight).parent(
+    canvasParentRef
+  );
+  p5.frameRate(this.fr);
+  // use parent to render canvas in this ref (without that p5 render this canvas outside your component)
+};
+const draw = p5 => {
+  p5.background(255);
+  p5.ellipse(50, 50, 70, 70);
+  p5.stroke("#ff0000");
+  p5.fill(255, 0, 0);
+};
+export default function App() {
+
+
+ 
+ 
+ return <Sketch setup={setup} draw={draw} />
 }
-
-export default App
